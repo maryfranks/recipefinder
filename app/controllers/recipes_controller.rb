213039bb307ae316c6698e6250688c10641f9659
Mary Franks <maryfranks@gmail.com>
2018-12-02 12:24:36 -0500

    display recipe results in table with three columns

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
+    @recipes = Recipe.get_recipes("https://www.food2fork.com/api/search?key=da315bf80dd50622e37f1677756d0a3d&q=chocolate")["recipes"]
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ActiveRecord::Base
+
+  def self.get_recipes(url)
+    response = HTTParty.get(url).body
+    JSON.parse(response)
+  end
+
 end
