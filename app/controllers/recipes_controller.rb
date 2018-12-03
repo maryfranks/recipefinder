a4f4a2d3c57549ef5723c5615c48624a9dd39009
Mary Franks <maryfranks@gmail.com>
2018-12-03 13:53:12 -0500

    update controller method and routes for search, accept search parameters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,13 @@
 class RecipesController < ApplicationController
 
-  def index
+  def search
     base_url = ENV["FOOD2FORK_SERVER_AND_PORT"]
     key = "key=#{ENV["FOOD2FORK_KEY"]}"
-    search_term = "chocolate"
+    if params[:search] == nil
+      search_term = "chocolate"
+    else
+      search_term = params[:search]
+    end
     url = "#{base_url}#{key}&q=#{search_term}"
     @recipes = Recipe.get_recipes(url)["recipes"]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'recipes#index'
+  root 'recipes#search'
+  get 'search' => 'recipes#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
